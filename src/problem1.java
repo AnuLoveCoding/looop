4941f1a55e83142116751f2af8fda75b4a3f6a43
anuLoveCoading <anusrivas07@gmail.com>
2022-11-06 02:09:58 +0530

    git commit -m "loop-problem"

--- src/problem1.java
+++ src/problem1.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class problem1 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        System.out.println("hii");
+
+        int n = scn.nextInt();
+        for (int i = 1; i <=n ; i++) {
+            System.out.print(i+" ");
+        }
     }
 }
