e59d62e9264dc2bbfde53ec9c3057bf411ecf09b
anuLoveCoading <anusrivas07@gmail.com>
2022-11-06 02:06:25 +0530

    git commit -m "loop-problem"

--- src/problem1.java
+++ src/problem1.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class problem1 {
+import java.util.Scanner;
+
+public class problem1 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("hii");
+    }
 }
