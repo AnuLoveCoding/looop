2b263085fe9cf5a7b3daa1d0d61c54ff5896dbe8
anuLoveCoading <anusrivas07@gmail.com>
2022-11-06 17:30:11 +0530

    git commit -m "loop-problem"

--- src/problem1.java
+++ src/problem1.java
@@ -25,17 +25,37 @@ public class problem1 {
 //        }
 
 //        Q.3 find the prime number;
+//            int count =0;
+//            for (int i = 1; i <=n ; i++) {
+//                if(n%i==0){
+//                    count++;
+//                }
+//                if(count>2){
+//                    break;
+//                }
+//            }
+//            if(count==2)System.out.println("It is prime number");
+//            else System.out.println("It is not prime number");
+
+//        Q.4 find the prime number 1 to n;
+
+        for (int i = 1; i <= n ; i++) {
             int count =0;
-            for (int i = 1; i <=n ; i++) {
-                if(n%i==0){
+            for (int j = 1; j <= i ; j++) {
+                if(i%j==0){
                     count++;
                 }
                 if(count>2){
                     break;
                 }
             }
-            if(count==2)System.out.println("It is prime number");
-            else System.out.println("It is not prime number");
+
+            if(count==2){
+                System.out.println("This is prime number "+i);
+//                System.out.println(i);
+            }
+        }
+
 
 
 
