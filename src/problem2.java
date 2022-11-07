8bb0c1f2636a1fe698148199ea30ca1ab76345f1
anuLoveCoading <anusrivas07@gmail.com>
2022-11-08 01:14:53 +0530

    git commit -m "loop-problem"

--- src/problem2.java
+++ src/problem2.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class problem2 {
     public static void main(String[] args) {
-        System.out.println("Hello Welcome loop");
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter Your number");
+        int n = scn.nextInt();
+
+        int count =0;
+
+        for (; n>0 ; n/=10) {
+             int rem = n%10;
+            count++;
+
+        }
+        System.out.print(count);
+
+
+
     }
 }
