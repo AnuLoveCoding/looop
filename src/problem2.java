f5c27f0942577288ebc299a2ac3a807fe78826aa
anuLoveCoading <anusrivas07@gmail.com>
2023-01-08 11:24:38 +0530

    git commit -m "loop-problem"

--- src/problem2.java
+++ src/problem2.java
@@ -3,17 +3,22 @@ import java.util.Scanner;
 public class problem2 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        System.out.println("Enter Your number");
-        int n = scn.nextInt();
+//        System.out.println("Enter Your number");
+//        int n = scn.nextInt();
+//
+//        int count =0;
+//
+//        for ( int  i=0; n>i ; n/=10) {
+//             int rem = n%10;
+//            count++;
+//
+//        }
+//        System.out.print(count);
 
-        int count =0;
-
-        for ( int  i=0; n>i ; n/=10) {
-             int rem = n%10;
-            count++;
-
-        }
-        System.out.print(count);
+//        (Infinite loop);
+//        for (int i = 0; 99 > 0; i--) {
+//            System.out.println("hello");
+//        }
 
 
 
