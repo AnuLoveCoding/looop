00e69dd4cea1d8a6cbf090b9fc4dc3e0f71710e6
anuLoveCoading <anusrivas07@gmail.com>
2023-01-29 11:59:28 +0530

    git commit -m "loop-problem"

--- src/car.java
+++ src/car.java
@@ -1,14 +1,18 @@
 public class car {
-    String name = "farrai";
-    int milage = 15;
-    String color = "red";
-    boolean is_sunroof = true;
+    String name ;
+    int  milage ;
+    String color ;
+    boolean is_sunroof ;
 
+    public  car(int milage, String name, String color, boolean is_sunroof){
+        this.milage = milage;
+        this.name = name;
+        this.color = color;
+        this.is_sunroof = is_sunroof;
 
-
-
+    }
 
     public static void main(String[] args) {
-
+          car farrai = new car (15,"farrai","red",true);
     }
 }
