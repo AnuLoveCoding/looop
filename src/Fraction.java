ac62f23974065c72e05a8f88ed499641773b710b
anuLoveCoading <anusrivas07@gmail.com>
2023-01-29 14:33:35 +0530

    git commit -m "loop-problem"

--- src/Fraction.java
+++ src/Fraction.java
@@ -24,10 +24,11 @@ public class Fraction {
         return new Fraction(0,0);
     }
 
-   Fraction multiply(Fraction a){
-        int i = (this.numerator * a.denominator + a.numerator * this.denominator) / this.denominator * a.denominator;
-        return new Fraction(0,0);
-    }
+   Fraction multiply(Fraction a) {
+       int i = (this.numerator * a.denominator + a.numerator * this.denominator) / this.denominator * a.denominator;
+       return new Fraction(0, 0);
+   }
+
     public static void main(String[] args) {
             Fraction x = new Fraction(2,3);
             Fraction y = new Fraction(4,5);
